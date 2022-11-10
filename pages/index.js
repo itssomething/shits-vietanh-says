7a147d36a48c8a10ba617a2a75d505bc68dd2e40
manh <vdmanh107@gmail.com>
2022-11-10 15:36:13 +0700

    fix

--- pages/index.js
+++ pages/index.js
@@ -4,38 +4,38 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <article id="53d5723c-ddbe-47c0-ad95-2ee8374b97f4" class="page sans">
+    <article id="53d5723c-ddbe-47c0-ad95-2ee8374b97f4" className="page sans">
       <header>
-        <div class="page-header-icon undefined"><span class="icon">💩</span></div>
-        <h1 class="page-title">shits viet anh says</h1>
+        <div className="page-header-icon undefined"><span className="icon">💩</span></div>
+        <h1 className="page-title">shits viet anh says</h1>
       </header>
-      <div class="page-body">
-        <ol type="1" id="d3b5ed02-2f0d-41fe-91ac-753914552c86" class="numbered-list" start="1">
+      <div className="page-body">
+        <ol type="1" id="d3b5ed02-2f0d-41fe-91ac-753914552c86" className="numbered-list" start="1">
           <li>nunya</li>
         </ol>
-        <figure id="b1b6e134-e2b8-4704-9ca2-9015c66a16a3" class="image"><a
-          href="#"><img style="width:301px"
+        <figure id="b1b6e134-e2b8-4704-9ca2-9015c66a16a3" className="image"><a
+          href="#"><img style={{ width: "301px" }}
             src="/Untitled.png" /></a></figure>
-        <ol type="1" id="e220074f-2899-48e1-bfef-2504aadefb1f" class="numbered-list" start="2">
+        <ol type="1" id="e220074f-2899-48e1-bfef-2504aadefb1f" className="numbered-list" start="2">
           <li>gay</li>
         </ol>
-        <figure id="03da325b-da34-4214-8eb0-345a94ab0135" class="image"><a
-          href="#"><img style="width:326px"
+        <figure id="03da325b-da34-4214-8eb0-345a94ab0135" className="image"><a
+          href="#"><img style={{ width: "326px"  }}
             src="/Untitled-1.png" /></a></figure>
-        <ol type="1" id="e95477cf-014b-4d07-b44f-d8911ddebf25" class="numbered-list" start="3">
+        <ol type="1" id="e95477cf-014b-4d07-b44f-d8911ddebf25" className="numbered-list" start="3">
           <li>tụt quần</li>
         </ol>
-        <figure id="b16d1dc0-c2d1-41b2-97b3-7c403cf788ec" class="image"><a
-          href="#"><img style="width:332px"
-            src="shits%20viet%20anh%20says%2053d5723cddbe47c0ad952ee8374b97f4/Untitled-2.png" /></a></figure>
-        <ol type="1" id="6984043c-961d-4b1c-a4ca-54b49d1697cf" class="numbered-list" start="4">
+        <figure id="b16d1dc0-c2d1-41b2-97b3-7c403cf788ec" className="image"><a
+          href="#"><img style={{ width: "332px" }}
+            src="/Untitled-2.png" /></a></figure>
+        <ol type="1" id="6984043c-961d-4b1c-a4ca-54b49d1697cf" className="numbered-list" start="4">
           <li>nứng</li>
         </ol>
-        <figure id="f9e38241-dee9-4086-97eb-3c69e2dd8a01" class="image"><a
-          href="#"><img style="width:310px"
-            src="shits%20viet%20anh%20says%2053d5723cddbe47c0ad952ee8374b97f4/Untitled-3.png" /></a></figure>
-        <figure id="880650b5-eca5-4183-a4c9-1fe8685d6ab0" class="image"><a
-          href="#"><img style="width:478px"
+        <figure id="f9e38241-dee9-4086-97eb-3c69e2dd8a01" className="image"><a
+          href="#"><img style={{ width: "310px" }}
+            src="/Untitled-3.png" /></a></figure>
+        <figure id="880650b5-eca5-4183-a4c9-1fe8685d6ab0" className="image"><a
+          href="#"><img style={{ width: "478px" }}
             src="shits%20viet%20anh%20says%2053d5723cddbe47c0ad952ee8374b97f4/Untitled-4.png" /></a></figure>
       </div>
     </article>
